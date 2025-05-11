7e122f205bc62f15d000ad4a6c0b2ab8c4802f05
MeetKhetani2003 <meetkhetani1111@gmail.com>
2025-05-11 18:06:09 +0530

    added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,9 +6,10 @@ import ContactPage from '@/components/Pages/ContactPage';
 import HomePage from '@/components/Pages/HomePage';
 import LayoutTemplate from '@/components/Templates/LayoutTemplate';
 import ServicePage from '@/components/Pages/ServicePage';
-import TeamPage from '@/components/Pages/TeamPage';
 
 import CustomCursor from './components/Molecuels/CustomCursor';
+import Projects from './components/Pages/Projects';
+import ServiceDetailedPage from './components/Pages/ServiceDetailedPage';
 
 function App() {
   return (
@@ -20,8 +21,12 @@ function App() {
             <Route path='/' element={<HomePage />} />
             <Route path='/about' element={<AboutPage />} />
             <Route path='/contact' element={<ContactPage />} />
-            <Route path='/service' element={<ServicePage />} />
-            <Route path='/team' element={<TeamPage />} />
+            <Route path='/services' element={<ServicePage />} />
+            <Route
+              path='/services/:service'
+              element={<ServiceDetailedPage />}
+            />
+            <Route path='/projects' element={<Projects />} />
           </Route>
         </Routes>
       </Router>
